0aec7536c7c19cf959ea92b0004aa34b49ab6cae
Exac <contact@thomasmclennan.ca>
2013-10-06 23:18:45 -0700

    Commit 1
    Add MainComponent.ccp
    Add MainComponent.h
    Add Game.cpp

---
+++ MainComponent.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include "MainComponent.h"
+
+using namespace std;
+
+int main()
+{
+	run();
+	return 0;
+}
+
+class MainComponent
+{
+public:
+	const static int WIDTH = 800;
+	const static int HEIGHT = 600;
+	
+};
+
+
+
+
+
+
+
+
+void run()
+{
+	MainComponent game;
+}
