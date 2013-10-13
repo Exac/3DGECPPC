a3bf267fde140049dde1706cd63fee6616fa665b
Exac <contact@thomasmclennan.ca>
2013-10-12 23:31:20 -0700

    Vector2f

--- Vector2f.cpp
+++ Vector2f.cpp
@@ -1,1 +1,11 @@
 #include "Vector2f.h"
+
+Vector2f::Vector2f()
+{
+	x = y = 0;
+}
+Vector2f::Vector2f(float x_, float y_)
+{
+	x = x_;
+	y = y_;
+}
--- Vector2f.h
+++ Vector2f.h
@@ -1,4 +1,13 @@
+#pragma once
+#include <string>
+using namespace std;
+
 class Vector2f
 {
-	//
+private:
+	float x;
+	float y;
+public:
+	Vector2f();
+	Vector2f(float, float);
 };
