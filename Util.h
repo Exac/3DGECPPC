46be5a0f54312985b1cfe138046597b121d59740
Exac <contact@thomasmclennan.ca>
2013-10-17 21:17:58 -0700

    Util outline

--- Util.h
+++ Util.h
@@ -3,5 +3,11 @@
 using namespace std;
 
 class Util{
-
+	//FloatBuffer createFloatBuffer(int size);
+	//IntBuffer createIntBuffer(int size);
+	//IntBuffer createFlippedBuffer();
+	//FloatBuffer createFlippedBuffer();
+	//FloatBuffer createFlippedBuffer(Matrix4f value);
+	//string removeEmptyStrings(string data);
+	//int toIntArray();
 };
