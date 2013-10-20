177d4e77d68b211f2385f4c709eccec25c4a16bd
Exac <contact@thomasmclennan.ca>
2013-10-19 22:43:29 -0700

    FloatBuffer header

--- FloatBuffer.h
+++ FloatBuffer.h
@@ -1,5 +1,24 @@
+//http://docs.oracle.com/javase/7/docs/api/java/nio/FloatBuffer.html
 class FloatBuffer{
 private:
 public:
+	FloatBuffer allocate(int capacity);
+	float array();
+	int arrayOffSet();
+	FloatBuffer asReadOnlyBuffer();
+	FloatBuffer compact();
+	int compareTo(FloatBuffer that);
+	FloatBuffer duplicate();
+	bool equals(FloatBuffer ob);
+	float get();
+	FloatBuffer get(float dst);
+	FloatBuffer get(float dst, int offset, int length);
+	float get(int index);
+	bool hasArray();
+	int hashCode();
+	bool isDirect();
+	//ByteOrder order();
+	FloatBuffer put(float f);
+	FloatBuffer put(float src[]);
 	FloatBuffer();
 };
